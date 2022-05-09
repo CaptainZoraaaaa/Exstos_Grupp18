2785ffb700d0834b9416f09436d5d6376590f27a
cyberknugen <102213145+cyberknugen@users.noreply.github.com>
2022-05-09 13:53:04 +0200

    logtextmodifikation

--- src/main/java/com/example/exstos_grupp18/KanbanViewController.java
+++ src/main/java/com/example/exstos_grupp18/KanbanViewController.java
@@ -32,91 +32,67 @@ public class KanbanViewController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    @FXML
-    private ProgressBar backlogBar; //progress bar for backlog
 
-    @FXML
-    private ScrollPane backlogSwimlane; //the swimlane for backlog
-
-    @FXML
-    private VBox backlogList; //list for task objects in the backlog swimlane
-
-    @FXML
-    private Button calendarButton; //the calendar button in the drop down menu
-
-    @FXML
-    private Button currentProjectButton; //the current project button on the drop down menu
-
-    @FXML
-    private ProgressBar doneBar; //progress bar for done
-
-    @FXML
-    private ScrollPane doneSwimlane; //swimlane for done
-
-    @FXML ListView<Object> doneList; //list for task objects in the done swimlane
-
-    @FXML
-    private ProgressBar dropMenuBar; //the progressbar in the drop down menu
-
-    @FXML
-    private Button helpButton; //the help button on the drop down menu
-
-    @FXML
-    private ProgressBar inProgressBar; //the progress bar for in progress
-
-    @FXML
-    private ScrollPane inProgressSwimlane; //the swimlane for in progress
-
-    @FXML
-    private VBox inProgressList; //list for task objects in the in progress swimlane
-
-    @FXML
-    private Button infoButton; //the info button on the drop down menu
-
-    @FXML
-    private Button kanbanButton; //the kanban button on the drop down menu
-
-    @FXML
-    private Button logOutButton; //the log out button
-
-    @FXML
-    private Button mainMenuButton; //main menu button
-
-    @FXML
-    private Button myProjectsButton; //"my projects button (which shows the drop down menu)
-
-    @FXML
-    private HBox myProjectsHbox; //the Hbox for the buttons in the drop down menu
-
-    @FXML
-    private Pane myProjectsMenu; //the pane the drop down menu is on.
-
-    @FXML
-    private Button newTaskButton; //button for new task
-
-    @FXML
-    private Button personalViewButton; //button for personal view
-
-    @FXML
-    private Button settingsButton; //settings-button on drop down menu
-
-    @FXML
-    private ProgressBar waitingBar; //the progress bar for waiting
-
-    @FXML
-    private ScrollPane waitingSwimlane; //the swimlane for waiting
-
-    @FXML
-    private ListView<Object> waitingList; //list for task objects in the waiting swimlane
-
-    @FXML
-    private Label usernameLabel; //the label for the username
     private ArrayList<Node> nodesInProgress = new ArrayList<>();
-
     private boolean dropMenuVisible = false;
-
     private Controller controller = Controller.getInstance();
     private static KanbanViewController yes = new KanbanViewController();
+
+    @FXML
+    private ProgressBar backlogBar; //progress bar for backlog
+    @FXML
+    private ScrollPane backlogSwimlane; //the swimlane for backlog
+    @FXML
+    private VBox backlogList; //list for task objects in the backlog swimlane
+    @FXML
+    private Button calendarButton; //the calendar button in the drop down menu
+    @FXML
+    private Button currentProjectButton; //the current project button on the drop down menu
+    @FXML
+    private ProgressBar doneBar; //progress bar for done
+    @FXML
+    private ScrollPane doneSwimlane; //swimlane for done
+    @FXML ListView<Object> doneList; //list for task objects in the done swimlane
+    @FXML
+    private ProgressBar dropMenuBar; //the progressbar in the drop down menu
+    @FXML
+    private Button helpButton; //the help button on the drop down menu
+    @FXML
+    private ProgressBar inProgressBar; //the progress bar for in progress
+    @FXML
+    private ScrollPane inProgressSwimlane; //the swimlane for in progress
+    @FXML
+    private VBox inProgressList; //list for task objects in the in progress swimlane
+    @FXML
+    private Button infoButton; //the info button on the drop down menu
+    @FXML
+    private Button kanbanButton; //the kanban button on the drop down menu
+    @FXML
+    private Button logOutButton; //the log out button
+    @FXML
+    private Button mainMenuButton; //main menu button
+    @FXML
+    private Button myProjectsButton; //"my projects button (which shows the drop down menu)
+    @FXML
+    private HBox myProjectsHbox; //the Hbox for the buttons in the drop down menu
+    @FXML
+    private Pane myProjectsMenu; //the pane the drop down menu is on.
+    @FXML
+    private Button newTaskButton; //button for new task
+    @FXML
+    private Button personalViewButton; //button for personal view
+    @FXML
+    private Button settingsButton; //settings-button on drop down menu
+    @FXML
+    private ProgressBar waitingBar; //the progress bar for waiting
+    @FXML
+    private ScrollPane waitingSwimlane; //the swimlane for waiting
+    @FXML
+    private ListView<Object> waitingList; //list for task objects in the waiting swimlane
+    @FXML
+    private Label usernameLabel; //the label for the username
+    @FXML
+    private Pane mainBarPane;
 
     public static KanbanViewController getInstance() {
         return yes;
@@ -132,17 +108,28 @@ public class KanbanViewController implements Initializable {
      usernameLabel.setText(username);
     }
 
+    /**
+     * @author Anna Håkansson
+     *
+     * Method that is called upon when the "My projects" button is pressed.
+     * boolean dropMenuVisible keeps track if the menu should be shown or
+     * diseapeared when the button is pressed.
+     */
     public void myProjectsPressed() {
         if(!dropMenuVisible) {
             dropMenuVisible = true;
-        } else {
+        }
+        else {
             dropMenuVisible = false;
         }
-        myProjectsMenu.setVisible(dropMenuVisible);
-        myProjectsMenu.setDisable(!dropMenuVisible);
+
+        myProjectsHbox.setVisible(dropMenuVisible);
+        myProjectsHbox.setDisable(!dropMenuVisible); //set HBox with buttons
+        mainBarPane.setVisible(dropMenuVisible);
+        mainBarPane.setDisable(!dropMenuVisible); //set the pane for the progress bar
     }
 
-
+    //TODO javadoca
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -156,21 +143,23 @@ public class KanbanViewController implements Initializable {
                     nodes[i].setId(String.valueOf(i));
                     //System.out.println(nodes[i].getId());
                     backlogList.getChildren().add(nodes[i]);
-                }else if (current.get(i).getCurrentStatus().equals(Swimlane.InProgress)){
+                }
+                else if (current.get(i).getCurrentStatus().equals(Swimlane.InProgress)){
                     nodes[i] = FXMLLoader.load(getClass().getResource("Task.fxml"));
                     Node scene = nodes[i];
                     nodes[i].setId(String.valueOf(i));
                     //System.out.println(nodes[i].getId());
                     inProgressList.getChildren().add(nodes[i]);
                 }
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
     }
 
-    public void skriv(String scene) {
+    public void skriv(String scene) { //TODO testmetod tas bort när den är färdiganvänd
         for (int i = 0; i < backlogList.getChildren().size(); i++) {
             if (backlogList.getChildren().get(i).equals(scene)){
                 System.out.println("här");
@@ -180,6 +169,7 @@ public class KanbanViewController implements Initializable {
         }
     }
 
+    //TODO javadoca
     public void newTask(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("newTask.fxml"));
         root = fxmlLoader.load();
