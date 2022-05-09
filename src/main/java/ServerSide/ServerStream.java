384860f8077a092681b237b1917cce14b1458337
Emma <34522599+monkeyontellus@users.noreply.github.com>
2022-05-09 08:16:36 +0200

    - ServerStreams in separate class files

--- src/main/java/ServerSide/ServerStream.java
+++ src/main/java/ServerSide/ServerStream.java
@@ -24,58 +24,7 @@ public class ServerStream {
      * creates and run two threads, one for output and one for input (from server)
      */
     private void StartThreads() {
-        new ServerReceiver().start();
-        new ServerSender().start();
+        new ServerReceiver(clientHandler, serverBuffer, socket).start();
+        new ServerSender(serverBuffer, socket).start();
     }
-
-    /**
-     * ServerReceiver holds a inputstream receiving information from server
-     */
-    public class ServerReceiver extends Thread{
-        private Package packageObject;
-
-        /**
-         * thread for receiving data server -> client.
-         * reads object with readObject() and save the data to a variable which then is
-         * passed along to a clientHandlet-method
-         */
-        @Override
-        public void run() {
-            while (!Thread.interrupted()){
-                try(ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()))) {
-                    packageObject = (Package) ois.readObject();
-                    clientHandler.packageRecieved(packageObject);
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * ServerSender hold outputstream which send information to server
-     * this thread is notified by the ServerBuffet when there is anything new to send, by using method get() we send last added
-     * object
-     */
-    public class ServerSender extends Thread{
-
-        /**
-         * run() runs the thread, while the thread isn't interrupted it will try to create the output-stream to the socket, it will
-         * write the message/package and flush() to make sure no data is lost when leaving the try. Since the stream is created within
-         * the try, there is no need to close it since it happen automatically when leaving the try-block
-         */
-        @Override
-        public void run() {
-            while (!Thread.interrupted()){
-                try(ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()))) {
-                    oos.writeObject(serverBuffer.get());//write latest object added in ServerBuffer
-                    oos.flush();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-
 }
