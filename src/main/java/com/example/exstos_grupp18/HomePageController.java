a3ae5e0de2818e4ee21784cea6f31109ae16287d
CaptainZoraaaaa <97522672+CaptainZoraaaaa@users.noreply.github.com>
2022-05-25 10:00:07 +0200

    Fixat popup x och y ounkt

--- src/main/java/com/example/exstos_grupp18/HomePageController.java
+++ src/main/java/com/example/exstos_grupp18/HomePageController.java
@@ -190,6 +190,9 @@ public class HomePageController implements Initializable {
                 projectList.getChildren().addAll(button);
             }
         System.out.println(LocalDate.now());
+        if (controller.getActiveProject() != null) {
+            projectName.setText(controller.getActiveProject().getProjectName());
+        }
     }
 
     /**
